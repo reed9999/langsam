daf09e733c1b3c62eaa10652831996d0604d2c26
Philip J. Reed <reed9999@users.noreply.github.com>
2021-10-15 23:57:36 +0000

    success in the semi-manual download JS

--- manual-helps/yucata-top-players.js
+++ manual-helps/yucata-top-players.js
@@ -1,13 +1,25 @@
-
-ranktbl = document.getElementById('rankingTable');
-console.log(ranktbl.innerText)
-// child 0 is some header not of interest
-innertbl = ranktbl.children[1];
-// for (row of innertbl.children) {
-for (i = 0; i < 10; i ++ ) {
-    row = innertbl.children[i];
-    
-    console.log(row.children[0].innerText)
-    console.log(row.children[2].innerHTML)
-    
+function getUserName(html) {
+    words = html.split(" ")
+    nameAndTag = words[words.length - 1]
+    name = nameAndTag.split("<")[0]
+    return name
 }
+
+function extractRankingList () {
+    ranktbl = document.getElementById('rankingTable');
+    console.log(ranktbl.innerText)
+    // child 0 is some header not of interest
+    innertbl = ranktbl.children[1];
+    for (row of innertbl.children) {
+        // row = innertbl.children[i];
+        
+        rank = row.children[0].innerText;
+        nameHTML = row.children[2].innerHTML;
+        
+        username = getUserName(nameHTML);
+        console.log (`Rank: ${rank} User ${username}`)
+        
+    }
+}
+
+extractRankingList()
